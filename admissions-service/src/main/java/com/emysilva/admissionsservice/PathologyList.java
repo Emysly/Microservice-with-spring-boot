1206baa29b63d48c0b02c6afbbf4ef1c191949fb
emysilva <emysly12@gmail.com>
2020-11-01 02:43:05 +0100

    create pathology list

---
+++ admissions-service/src/main/java/com/emysilva/admissionsservice/PathologyList.java
@@ -0,0 +1,14 @@
+package com.emysilva.admissionsservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PathologyList {
+    private List<Pathology> pathologyList;
+}
