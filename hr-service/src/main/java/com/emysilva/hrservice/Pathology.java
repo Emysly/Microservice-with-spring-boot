c95813a653760f1b438528a45ce8d21abd2d1c3a
emysilva <emysly12@gmail.com>
2020-11-01 02:42:52 +0100

    create pathology entity

---
+++ hr-service/src/main/java/com/emysilva/hrservice/Pathology.java
@@ -0,0 +1,14 @@
+package com.emysilva.hrservice;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pathology {
+    private Integer id;
+    private String disease;
+}
