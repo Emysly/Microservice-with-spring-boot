55bd1099ce29a377283e184f761b8b9440537314
emysilva <emysly12@gmail.com>
2020-11-01 02:42:01 +0100

    create employee entity

---
+++ pathology-service/src/main/java/com/emysilva/pathologyservice/Employee.java
@@ -0,0 +1,15 @@
+package com.emysilva.pathologyservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String specialty;
+}
